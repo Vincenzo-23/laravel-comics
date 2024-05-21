9be08005651f8b6e41697a5332b32bfe23970004
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-21 13:35:51 +0200

    ADD: created header and footer inside the partials folder

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <ul class="row">
+            <li class="col-3">About us</li>
+            <li class="col-3">Socials</li>
+            <li class="col-3">Contacts</li>
+            <li class="col-3">Work with us</li>
+        </ul>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="container">
+        <ul class="row">
+            <li>Comics</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Contacts</li>
+        </ul>
+    </nav>
+</header>
